6f08f8322a8887ed98a5d35835d2d4bbc8fe8ff4
Jpanaza1906 <3848221850101@ingenieria.usac.edu.gt>
2024-04-09 23:32:00 -0600

    dashcompleto

--- Proyecto1/frontend/src/pages/dashboard.js
+++ Proyecto1/frontend/src/pages/dashboard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Card from '../components/card'; // Asegúrate de importar tu componente Card aquí
 import HeatMapChart from '../components/heatmapchart';
-import ComboBox from '../components/combobox';
 import '../css/dashboard.css';
 
 const Dashboard = () => {
@@ -12,7 +11,7 @@ const Dashboard = () => {
         for (let i = 0; i < 15; i++) {
             const row = [];
             for (let j = 0; j < 15; j++) {
-                const randomNumber = Math.floor(Math.random() * 11); // Genera un número aleatorio entre 0 y 10
+                const randomNumber = Math.floor(Math.random() * 99); // Genera un número aleatorio entre 0 y 10
                 //row.push(j);
                 row.push(randomNumber);
             }
@@ -35,32 +34,45 @@ const Dashboard = () => {
                     </Col>
                     <Col lg={4} md={12}>
                         <Row>
-                            <Card title="Room Detail" width="100%" height="45vh">
+                            <Card title="House Diagram" width="100%" height="42vh">
                                 {/* Contenido de la tarjeta pequeña */}
-                                <ComboBox
-                                    options={['Room 1', 'Room 2', 'Room 3']}
-                                    placeholder="Select a room"
-                                />
-                                <div className='prop'>
-                                    <div>
-                                        <button id='btnprop'>Time</button>
-                                        <button id='btnprop'>{"00:00:00"}</button>
+                                {/* img src={diagram} alt="House Diagram" style={{ width: '100%', height: '30vh' }} />*/}
+                                <div className='house'>
+                                    <div className='room1'>
+                                        <button id='btnr'>ROOM 1</button>
                                     </div>
-                                    <div>
-                                        <button id='btnprop'>Type</button>
-                                        <button id='btnprop'>{"Bathroom"}</button>
+                                    <div className='room'>
+                                        <button id='btnr'>ROOM 2</button>
+                                        <button id='btnr'>ROOM 3</button>
                                     </div>
-                                    <div>
-                                        <button id='btnprop'>State</button>
-                                        <button id='btnprop'>{"Occupied"}</button>
+                                    <div className='room'>
+                                        <button id='btnr'>ROOM 4</button>
+                                        <button id='btnr'>ROOM 5</button>
                                     </div>
                                 </div>
-
                             </Card>
                         </Row>
                         <Row>
-                            <Card title="House Diagram" width="100%" height="38vh">
+                            <Card title="Room Detail" width="100%" height="41vh">
                                 {/* Contenido de la tarjeta pequeña */}
+                                <div className='prop'>
+                                    <div>
+                                        <button id='btntitle'>Room</button>
+                                        <button id='btnprop'>{"N"}</button>
+                                    </div>
+                                    <div>
+                                        <button id='btntitle'>Time</button>
+                                        <button id='btnprop'>{"00:00:00"}</button>
+                                    </div>
+                                    <div>
+                                        <button id='btntitle'>Type</button>
+                                        <button id='btnprop'>{"Bathroom"}</button>
+                                    </div>
+                                    <div>
+                                        <button id='btntitle'>State</button>
+                                        <button id='btnprop'>{"Occupied"}</button>
+                                    </div>
+                                </div>
 
                             </Card>
                         </Row>
